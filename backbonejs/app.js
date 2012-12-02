6cda39fea8a04cdc9197f274e781307e842c9bb8
Emanuele DelBono <emanuele@codiceplastico.com>
2012-12-02 07:42:37 +0100

    playing with backbone

--- backbonejs/app.js
+++ backbonejs/app.js
@@ -3,3 +3,61 @@ MyApp = {
         console.log('all is up and running');
     }
 }
+
+// MODEL
+
+Person = Backbone.Model.extend({
+        defaults: {
+            name: 'defautl_name'
+        },
+        initialize: function(){
+            this.on("change:name", function(model){
+                var name = model.get('name'); 
+                console.log("Changed my name to " + name );
+            });
+        }
+
+    });
+    
+var person = new Person();
+
+// ROUTER
+
+var AppRouter = Backbone.Router.extend({
+        routes: {
+            "do1/:id": "route1",
+            "do2/:id": "route2" 
+        }
+    });
+    // Initiate the router
+    var app_router = new AppRouter;
+
+    app_router.on('route:route1', function(actions) {
+        console.log('route1');
+        console.log(actions);
+    });
+
+    app_router.on('route:route2', function(actions) {
+        console.log('route2');
+        console.log(actions);
+    })
+
+    Backbone.history.start();
+
+SearchView = Backbone.View.extend({
+        initialize: function(model){
+            this.render();
+        },
+        render: function(){
+            var template = _.template( $("#search_template").html(), {search: 'ricerca'} );
+            this.$el.html( template );
+        },
+        events: {
+            "click input[type=button]": "doSearch"
+        },
+        doSearch: function( event ){
+            console.log( "Search for " + $("#search_input").val() );
+        }
+    });
+
+var search_view = new SearchView({ model: person, el: $("#search_container") });
