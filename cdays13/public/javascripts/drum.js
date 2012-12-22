16559c9a3a70ebfb78791d534107963942bfcbf5
Emanuele DelBono <emanuele@codiceplastico.com>
2012-12-22 23:43:10 +0100

    rename e moved some files

--- cdays13/public/javascripts/drum.js
+++ cdays13/public/javascripts/drum.js
@@ -1,20 +1,6 @@
 $(function(){
     window.context = new webkitAudioContext()
-    window.oscillator = null;
-
-    $(document).on('click', '.play', function(){
-        var frequency = $(this).data('frequency');
-
-        oscillator = context.createOscillator();
-        oscillator.type = 3; // sine wave
-        oscillator.frequency.value = frequency;
-        oscillator.connect(context.destination);
-
-        oscillator.noteOn(0);
-        oscillator.noteOff(200);
-        setInterval(function(){oscillator.noteOff(0)}, 1000);
-    });
-
+    
     $(document).on('click', '.drum', function(){
         var sound = $(this).data('sound');
         playSound(sound);
