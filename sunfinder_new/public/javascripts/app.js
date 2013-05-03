1a860e9a5140d89a64291496198b2a0384963458
Emanuele DelBono <emanuele@codiceplastico.com>
2013-05-03 18:37:49 +0200

    it works (...but it should be better)

--- sunfinder_new/public/javascripts/app.js
+++ sunfinder_new/public/javascripts/app.js
@@ -5,8 +5,13 @@ $(function(){
 
 		console.log('yo');
 		$.get('/weather?lat=' + lat + '&lon=' + lon, function(response){
-			console.log('finish');
 			console.log(response);
+			$('#items').html('');
+			$(response).each(function(i, item){
+                var element = '<li>' + item.name + '&nbsp;&nbsp;<span>' + item.condition + '&nbsp;&nbsp;</span><small>' + item.clouds +'</small></li>';
+                
+                $('#items').append(element);
+            });
 		});
 
 		return false;
--- sunfinder_new/routes/index.js
+++ sunfinder_new/routes/index.js
@@ -13,12 +13,10 @@ exports.weather = function(req, res){
 	MongoClient.connect("mongodb://localhost:27017/sunfinder", function(err, client) {
 	    var collection = client.collection('cities');
 	    
-	    var lat = req.query['lat'];
-	    var lon = req.query['lon'];
+	    var lat = parseFloat(req.query['lat']);
+	    var lon = parseFloat(req.query['lon']);
 
-	   
-
-	    var distance = 800;
+	    var distance = 1000;
 
 	    var results = [];
 	    var semaphore = true;
@@ -26,34 +24,45 @@ exports.weather = function(req, res){
 	    cursor = collection
 	    	.find( {loc:{$near: [lat, lon], $maxDistance: distance/111.12}}).limit(5)
 	    	.toArray(function(err, docs){
+	    	
+	    	var max = docs.length;
+	    	var count = 0;
+
+	    	_.each(docs, function(doc){
 	    		
-	    		async.eachSeries(docs, function(doc){
-	    			
-					if (doc != null)
-			        {
-			        	console.log('### get weather');
-			        	
-			        	var wheaterInfo = getWeather(doc, function(wheaterInfo){
-			        		results.push(wheaterInfo);	
-			        		console.log('### got it', results.length);
-			        	});
-			        }
-	    		}, function(err){
-	    			console.log('### completed');
-	    			client.close();
-	    			res.send(results);
-	    		});
-	    	});
+	    		if (doc != null)
+			    {   	
+			       	var wheaterInfo = getWeather(doc, function(wheaterInfo){
+						count ++;
+						if (wheaterInfo != null){
+							results.push(wheaterInfo);	
+							console.log(count, wheaterInfo);
+							if (count === max){
+								client.close();
+								res.send(results);
+							}
+						}
+
+		        	});
+		        }
+				else{
+					count ++;
+				}
+    			
+    		});
+    	});
 	});
 };
 
 function getWeather(doc, callback){
 	var  url = "http://api.openweathermap.org/data/2.5/weather?lat="+ doc.loc[0] + "&lon=" + doc.loc[1] + "&units=metric&lang=it";
-    rest.get(url).on('complete', function(result){
+    rest.get(url).on('complete', function(result) {
         var meteo = JSON.parse(result);
-        //if (meteo.clouds.all < 40){
-        console.log('### callback');
-        callback({name: meteo.name, condition: meteo.weather[0].main});
-        //}                
+        if (parseInt(meteo.clouds.all) < 40){
+        	callback({name: meteo.name, condition: meteo.weather[0].main, clouds: meteo.clouds.all});
+        }        
+		else{
+			callback(null);
+		}
     });
 }
