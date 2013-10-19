d82d655bd6433cc5be8ea5bcc3c5e74fb47814b2
Emanuele DelBono <emanuele@codiceplastico.com>
2013-10-19 18:39:23 +0200

    various

--- angular/js/DirectivesController.js
+++ angular/js/DirectivesController.js
@@ -3,7 +3,9 @@
 var myApp = angular.module('TestAngular');
 
 myApp.controller('DirectivesController', ['$scope', function ($scope) {
-	$scope.collection = [{name: 'uno', cc:{value: 10}}, {name:'due', cc:{value: 11}}];
+	$scope.collection = [
+		{name: 'uno', cc:{value: 10}}, {name:'due', cc:{value: 11}}
+	];
 	
 }]);
 
@@ -11,11 +13,10 @@ myApp.directive('itemHeader', [function() {
     return {
         restrict: 'A',
         scope: {
-            name: "=",
-			cc: "="
+			item:"=itemHeader"
         },
         link: function(scope, elem, attrs) {
-            console.log('dir', scope.name, scope.cc);
+            console.log('dir', scope.item, scope.$parent.item);
         }
     };
 }]);
--- node-tests/async.js
+++
@@ -1,6 +0,0 @@
-var events = require('events');
-var emitter = new events.EventEmitter();
-
-emitter.on('yo', function(){console.log('yo', this);});
-
-emitter.emit('yo');
