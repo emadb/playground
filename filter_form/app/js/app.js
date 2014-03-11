dd09b17f3f1ebeeca3cafa677d6bdfe101b502f5
Emanuele DelBono <emanuele@codiceplastico.com>
2014-03-11 17:48:26 +0100

    go on with the directive

--- filter_form/app/js/app.js
+++ filter_form/app/js/app.js
@@ -6,28 +6,48 @@ window.app.controller('FilterController', ['$scope', '$http', function ($scope, 
 }]);
 
 window.app.directive('filters',['$http', function($http) {
+
+    function applyWatcher(scope, data, index){
+        scope.$watch(['filters[', index, '].selectedField'].join(''), function(newValue, oldValue) {
+            console.log('change', newValue);
+            var selected = _.find(data, function(item){
+                return item.name === newValue;
+            });        
+            if (selected !== undefined) {
+                scope.values = selected.values;      
+            }
+        });
+    }
+
+    function bindFields(scope, data, index){
+        scope.filters[index].fields = _.map( data, function(item){
+            return item.name;
+        }); 
+    }
+
     return {
         restrict: 'E',
         templateUrl: 'filterTemplate.html',
+        
         link: function(scope, elem, attrs) {
             var data;
+            scope.filters = [
+                {selectedField:'', selectedOperator:'', selectedValue:''}
+                ];
             scope.operators = ['=', '!=', 'empty'];
             scope.selectedField = '';
 
-            scope.$watch('selectedField', function(newValue, oldValue) {
-                var selected = _.find(data, function(item){
-                    return item.name === newValue;
-                })        
-                if (selected !== undefined) {
-                    scope.values = selected.values;      
-                }
-            });
+            applyWatcher(scope, data, 0);
+
+            scope.newLine = function(){
+                scope.filters.push({selectedField:'', selectedOperator:'', selectedValue:''});
+                applyWatcher(scope, data, scope.filters.length - 1);
+                bindFields(scope, data, scope.filters.length - 1);
+            };
 
             $http.get('/js/filterMetadata.js').then(function(meta){
                 data = meta.data;
-                scope.fields = _.map( data, function(item){
-                    return item.name;
-                });
+                bindFields(scope, data, 0);
             });
         }
     };
