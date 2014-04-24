aef28c0f0a6c8e608d7f9b3a36f178282a3fbac2
Emanuele DelBono <emanuele@codiceplastico.com>
2014-04-24 08:18:45 +0200

    build

--- trim/gulpfile.js
+++ trim/gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 var paths = {
   scripts: ['app/js/app.js'],
-  scss: ['app/css/style.scss']
+  scss: ['app/css/style.scss'],
+  build: './build'
 };
 
 gulp.task('lint', function() {
@@ -30,4 +32,13 @@ gulp.task('watch', function() {
   gulp.watch(paths.scss, ['sass']);
 });
 
+gulp.task('copy', function(){
+  gulp.src(['./app/*.html']).pipe(gulp.dest(paths.build));
+  gulp.src(['./app/js/*.js'])
+    .pipe(gulp.dest(paths.build + '/js/'))
+    .pipe(concat('all.min.js'));
+  gulp.src(['./app/css/*.css']).pipe(gulp.dest(paths.build + '/css/'));
+});
+
 gulp.task('default', ['connect', 'watch', 'lint', 'sass']);
+gulp.task('build', ['lint', 'sass', 'copy']);
