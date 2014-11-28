b7c25e2da01c911e9debd44da5ec40aafd9c14f9
Emanuele DelBono <emanuele@codiceplastico.com>
2014-11-28 10:50:53 +0100

    with inline data

--- react-0.12.1/tute/app.js
+++ react-0.12.1/tute/app.js
@@ -1,10 +1,11 @@
+var converter = new Showdown.converter();
 
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -13,9 +14,30 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+         <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
@@ -31,10 +53,13 @@ var CommentForm = React.createClass({
   }
 });
 
+var data = [
+  {author: "Pete Hunt", text: "This is one comment1"},
+  {author: "Jordan Walke", text: "This is *another* comment2"}
+];
 
-React.render(
-  
-  <CommentBox/>,
-  
+
+React.render(  
+  <CommentBox data={data}/>,
   document.getElementById('content')
 );
