c6829361936b9f7165a2239255dcfb326e5c957a
Emanuele DelBono <emanuele@codiceplastico.com>
2014-11-28 10:59:17 +0100

    ajax

--- react-0.12.1/tute/app.js
+++ react-0.12.1/tute/app.js
@@ -1,11 +1,26 @@
 var converter = new Showdown.converter();
 
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -60,6 +75,6 @@ var data = [
 
 
 React.render(  
-  <CommentBox data={data}/>,
+  <CommentBox url="/tute/comments.json"/>,
   document.getElementById('content')
 );
